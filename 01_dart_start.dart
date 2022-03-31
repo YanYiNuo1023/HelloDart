45ac6e9379edc6030fe33140b84ba5174936f665
YanYiNuo1023 <1067212315@qq.com>
2022-03-31 09:10:13 +0000

    增加了“函数“部分

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -37,8 +37,18 @@ void control() {
   }
 }
 
+//函数（https://dart.cn/samples#functions）
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 void main() {
   //变量
   variables();
+  //流程控制语句
   control();
+  //函数
+  var result = fibonacci(20);
+  print('result = $result');
 }

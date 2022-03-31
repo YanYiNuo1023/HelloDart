4a6c0d33a88c48f529653867712e96546e14165e
YanYiNuo1023 <1067212315@qq.com>
2022-03-31 08:56:43 +0000

    增加流程控制语句

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -13,7 +13,32 @@ void variables() {
       'name is $name, year is $year, antennaDiameter is $antennaDiameter, flybyObjects is $flybyObjects, image is $image');
 }
 
+//流程控制语句（https://dart.cn/samples#control-flow-statements）
+void control() {
+  var year = 1977;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Urans', 'Neptune'];
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+}
+
 void main() {
   //变量
   variables();
+  control();
 }

9cf56f8ff8ae72a44864f66c129dc6d891848679
YanYiNuo1023 <1067212315@qq.com>
2022-06-19 05:00:19 +0000

    增加“异步”部分

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -1,5 +1,7 @@
 //导入（https://dart.cn/samples#imports）
 import 'dart:math';
+import 'dart:async';
+import 'dart:io';
 
 //变量 （https://dart.cn/samples#variables）
 void variables() {
@@ -219,7 +221,58 @@ void interface_and_abstract_classes() {
   voyager5.describe();
 }
 
-void main() {
+//异步（https://dart.cn/samples#async）
+Future<void> the_async() async {
+  print('\n');
+  print('*' * 50);
+  print('异步');
+  print('*' * 50);  
+
+  const oneSecond = Duration(seconds: 1);
+
+  Future<void> printWithDelay1(String message) async {
+    await Future.delayed(oneSecond);
+    print(message);
+  }
+
+  printWithDelay1('过了1秒钟.1');
+  print('done 1.');
+
+  Future<void> printWithDelay2(String message) {
+    return Future.delayed(oneSecond).then((_) {
+      print(message);
+    });
+  }
+
+  printWithDelay2('过了1秒钟.2');
+  print('done 2.');
+
+  Future<void> createDescriptions(Iterable<String> objects) async {
+    for (final object in objects) {
+      try {
+        var file = File('$object.txt');
+        if (await file.exists()) {
+          var modified = await file.lastModified();
+          print(
+              'File for $object already exists. It was modified on $modified.');
+          continue;
+        }
+        await file.create();
+        await file.writeAsString('Start Describing $object in this file.');
+        print('File for $object created.');
+      } on IOException catch (e) {
+        print('Cannot create description for $object: $e');
+      }
+    }
+  }
+
+  var objects = ['Jupiter', 'Saturn', 'Urans', 'Neptune'];
+  createDescriptions(objects);
+
+  await Future.delayed(Duration(seconds: 5));
+}
+
+Future<void> main(List<String> args) async {
   //变量
   variables();
   //流程控制语句
@@ -238,4 +291,6 @@ void main() {
   mixins();
   //接口和抽象类
   interface_and_abstract_classes();
+  // 异步
+  await the_async();
 }
